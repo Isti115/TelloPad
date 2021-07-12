91434416a996e38e671023c9057d9143eec3e0ed
Donkó István <istvan.donko@gmail.com>
2021-07-12 19:23:52 +0200

    Initial commit (Auto generated project files)

---
+++ app/src/main/java/hu/isti115/tellopad/MainActivity.kt
@@ -0,0 +1,11 @@
+package hu.isti115.tellopad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
